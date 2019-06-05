925833c666870ebca1a8e08f22d61819daa33186
siddhartha behera <siddharthabehera@yahoo.co.in>
2019-06-06 01:21:14 +0530

    Har coded values moved to static

--- src/com/bjss/PriceBasket.java
+++ src/com/bjss/PriceBasket.java
@@ -15,6 +15,12 @@ public class PriceBasket {
     //injecting price service
     private static final PriceManagerService pc = new PriceManagerService();
     private static final ResourceBundle resource = ResourceBundle.getBundle("Messages");
+    private static final String APPLE = "Apple";
+    private static final String BREAD = "Bread";
+    private static final String SOUP = "Soup";
+    private static final int FIFTY = 50;
+    private static final int TEN = 10;
+
 
     public static void main(String[] args) {
 
@@ -33,8 +39,7 @@ public class PriceBasket {
             try {
                 //check if the item passed as an argument exist in our catalogue
                 if(pc.getPrice(new Item(arg)) == null){
-
-                    String errorMessage = resource.getString("data_item_catalogue");
+                    String errorMessage = resource.getString("err_invalid_item");
                     throw new ItemNotFoundException(String.format(errorMessage, arg));
                 }
                 cartItems.add(new Item(arg));
@@ -50,10 +55,10 @@ public class PriceBasket {
         PriceCalculator pc = new SubTotalCalculatorService();
 
         //Apples have 10% off their normal price this week
-        pc = new PercentDiscountService(pc, new Item("Apple"), 10);
+        pc = new PercentDiscountService(pc, new Item(APPLE), TEN);
 
         //Buy 2 tins of soup and get a loaf of bread for half price
-        pc = new SecondDiscountService(pc, new Item("Soup"), 2, new Item("Bread"), 50);
+        pc = new SecondDiscountService(pc, new Item(SOUP), 2, new Item(BREAD), FIFTY);
 
 
         System.out.println("************** PRICE BASKET ************* ");
